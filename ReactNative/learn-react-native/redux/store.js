26a47242e7fcadcf5ef657ef8816e78f7d5d7a61
Kyhoon <125944696+EndingsHUB@users.noreply.github.com>
2025-09-15 08:49:31 +0900

    Delete ReactNative directory

--- ReactNative/learn-react-native/redux/store.js
+++
@@ -1,8 +0,0 @@
-﻿import {configureStore} from "@reduxjs/toolkit";
-import todoReducer from './slices/todoSlice';
-
-export const store = configureStore({
-    reducer: {
-        todo: todoReducer
-    }
-})
